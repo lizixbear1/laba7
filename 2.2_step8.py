d9d9115c65e30b8bd4e16118c1ac2e41dfef6a3a
Elizabeth <me.liza859@gmail.com>
2019-12-21 23:14:00 +0700

    Готовый 2.2_step8

--- 2.2_step8.py
+++ 2.2_step8.py
@@ -8,30 +8,32 @@ time.sleep(2) # Пауза 2 секунды
 
 # Переход по ссылке:
 driver.get("http://suninjuly.github.io/file_input.html") 
-#time.sleep(2)
+#time.sleep(5)
 
 # Ищем строку, в которую нужно вписать свое имя:
 name=driver.find_element_by_name("firstname")
-
 # Пишем в строку свое имя:
 name.send_keys("Elizabeth")
 
 # Ищем строку, в которую нужно вписать свою фамилию:
 lastname=driver.find_element_by_name("lastname")
-
 # Пишем в строку свлю фамилию:
 lastname.send_keys("Medvedeva")
 
 # Ищем строку, в которую нужно вписать свой email:
 email=driver.find_element_by_name("email")
-
 # Пишем в строку свою почту:
 email.send_keys("me.liza@bk.ru")
 
-# Загружаем текстовый файл:
+# Находим кнопку "Выберете файл":
+file = driver.find_element_by_css_selector('#file')
+# Получаем путь к директории текущего исполняемого файла:
 current_dir = os.path.abspath(os.path.dirname(__file__))
+# Добавляем к этому пути имя файла:
 file_path = os.path.join(current_dir, 'file.txt')
+# Добавляем файл:
+file.send_keys(file_path)
 
 # Находим элемент кнопки "Submit":
 submit_button=driver.find_element_by_css_selector(".btn")
-submit_button.click() # Нажимаем на кнопку
+submit_button.submit() # Нажимаем на кнопку
